d834e20c236f8c80490d2296abd093a8bc13228e
asnock <61778244+asnock@users.noreply.github.com>
2020-04-02 19:16:51 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,62 @@
+function calculatePrice() {
+    var hairDyePrice = 3.37; //1ml
+    var oxygenPrice = 0.35; //1ml
+    var shampooPrice = 0.35; //1ml
+    var conditionerPrice = 0.46; //1ml
+    var towelPrice = 2.5; //1 item
+    var glovesPrice = 3; //1 pair
+    var powderPrice = 1; //1g
+    var foilPrice = 0.8; //1m
+    var customerFoilPrice = 2; //1m
+    hairLength  = document.getElementById("hairLength").value;
+    switch (hairLength) {
+        case "length2":
+            price = (shampooPrice * 30) + (conditionerPrice * 20) + glovesPrice; 
+            break;
+        case "length3":
+            price = (shampooPrice * 50) + (conditionerPrice * 20) + glovesPrice;
+            break;  
+        case "length4":
+            price = (shampooPrice * 60) + (conditionerPrice * 40) + glovesPrice;
+            break;
+        case "length5":
+            price = (shampooPrice * 70) + (conditionerPrice * 50) + glovesPrice;
+            break;
+        default:
+            price = (shampooPrice * 20) + (conditionerPrice * 10) + glovesPrice;
+    }
+    switch (hairLength) {
+        case "length2":
+            customerPrice = (shampooPrice * 30 * 2.86) + (conditionerPrice * 20 * 2.86) + (glovesPrice * 2); 
+            break;
+        case "length3":
+            customerPrice = (shampooPrice * 50 * 2.86) + (conditionerPrice * 20 * 2.86) + (glovesPrice * 2);
+            break;  
+        case "length4":
+            customerPrice = (shampooPrice * 60 * 2.86) + (conditionerPrice * 40 * 2.86) + (glovesPrice * 2);
+            break;
+        case "length5":
+            customerPrice = (shampooPrice * 70 * 2.86) + (conditionerPrice * 50 * 2.86) + (glovesPrice * 2);
+            break;
+        default:
+            customerPrice = (shampooPrice * 20 * 2.86) + (conditionerPrice * 10 * 2.86) + (glovesPrice * 2);
+    }
+    hairDyeQuantity = document.getElementById("hairDyeQuantity").value;
+    foilQuantity  = document.getElementById("foilQuantity").value;
+    towelQuantity = document.getElementById("towelQuantity").value;
+    oxygenQuantity = document.getElementById("oxygenQuantity").value;
+    powderQuantity = document.getElementById("powderQuantity").value;
+    if(foilQuantity == ""){
+        alert("Вы не указали количество фольги");
+    } else if(towelQuantity == ""){
+        alert("Вы не указали количество полотенец");
+    } else {
+        finalPrice = parseFloat (foilQuantity * foilPrice) + parseFloat (towelQuantity * towelPrice) + parseFloat (hairDyeQuantity * hairDyePrice) + 
+        parseFloat (oxygenQuantity * oxygenPrice) + parseFloat (powderQuantity + powderPrice) + price;
+        document.getElementById("finalPrice").innerHTML = "Расчетная стоиомсть: "+ finalPrice +" грн.";
+        customerFinalPrice = parseFloat (foilQuantity * customerFoilPrice) + parseFloat (towelQuantity * towelPrice * 2) +
+         parseFloat (hairDyeQuantity * hairDyePrice * 2) + parseFloat (oxygenQuantity * oxygenPrice * 2) + 
+         parseFloat (powderQuantity + powderPrice * 2) + customerPrice ;
+        document.getElementById('customerFinalPrice').innerHTML = "Стоимость для клиента: "+ customerFinalPrice +" грн.";
+    }
+}
